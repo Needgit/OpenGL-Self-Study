afc6181b6c89f8632f92dc116c82afaf534fbf93
nicch13 <nicch13@student.sdu.dk>
2018-05-11 12:02:44 +0200

    Master

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,43 +4,22 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../01Window.cpp \
-../02Triangle.cpp \
-../03Shaders1.cpp \
-../03Shaders2.cpp \
-../04Texture.cpp \
-../05Transformation.cpp \
-../06Camera.cpp \
 ../GLcamera.cpp \
 ../GLobject.cpp \
 ../GLshader.cpp \
-../main.cpp 
+../Main.cpp 
 
 OBJS += \
-./01Window.o \
-./02Triangle.o \
-./03Shaders1.o \
-./03Shaders2.o \
-./04Texture.o \
-./05Transformation.o \
-./06Camera.o \
 ./GLcamera.o \
 ./GLobject.o \
 ./GLshader.o \
-./main.o 
+./Main.o 
 
 CPP_DEPS += \
-./01Window.d \
-./02Triangle.d \
-./03Shaders1.d \
-./03Shaders2.d \
-./04Texture.d \
-./05Transformation.d \
-./06Camera.d \
 ./GLcamera.d \
 ./GLobject.d \
 ./GLshader.d \
-./main.d 
+./Main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
--- Main.cpp
+++ Main.cpp
@@ -1,110 +1,216 @@
-#include <stdio.h>
-
 #include <GL/glew.h>
 #include <GL/glfw3.h>
 
-#define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
-static GLint width, height;
+#include <iostream>
+#include <math.h>
 
-GLfloat vertices[4][3] = {
-	{-0.5f, -0.5f, 0.0f},
-	{ 0.5f, -0.5f, 0.0f},
-	{ 0.0f,  0.5f, 0.0f},
-	{ 0.0f,  0.5f, 0.5f}
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "GLshader.h"
+#include "GLcamera.h"
+
+
+static GLFWwindow* window;
+
+static GLshader shader;
+static GLcamera camera;
+
+static GLuint VAO;
+static GLuint VBO;
+
+static GLuint texture1, texture2;
+
+static GLfloat vertices[] = {
+//  Vertex:					Color:				   Tex Coord:
+    -0.5f, -0.5f, 0.0f, 	 1.0f,  0.0f,  0.0f,	0.1f,  0.1f,
+     0.5f, -0.5f, 0.0f,		 0.0f,  1.0f,  0.0f,	0.9f,  0.1f,
+     0.0f,  0.5f, 0.0f,		 0.0f,  0.0f,  1.0f,	0.51f, 0.925f,
 };
 
-GLfloat colors[4][3] = {
-	{ 1, 0, 0},
-	{ 0, 1, 0},
-	{ 0, 0, 1},
-	{ 1, 1, 1}
-};
 
-static GLfloat angle = 0;
-
-
-void errorCallback(int error, const char* description)
+void loadImage(const GLchar* path, GLuint &TBO)
 {
-    fprintf(stderr, "Error: %s\n", description);
+	// Flip y-axis so positive is upwards:
+	stbi_set_flip_vertically_on_load(true);
+
+	// Load Image Data using STB Library:
+	int width, height, nrChannels;
+	unsigned char *image = stbi_load(path, &width, &height, &nrChannels, 0);
+	if (image)
+	{
+		// Create OpenGL Texture Obejct:
+		glGenTextures(1, &TBO);
+		glBindTexture(GL_TEXTURE_2D, TBO);
+
+		// Load Image Data into Object:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D); // Generate a Mipmap of texture;
+	}
+	else
+	{
+		 std::cout << "Failed to load texture..." << std::endl;
+	}
+
+	// Cleanup Image Data:
+	stbi_image_free(image);
 }
 
 
-void display(void)
+void onStart()
 {
-    GLuint i;
+	glEnable(GL_DEPTH_TEST);
 
+	shader = GLshader("Shaders/05Transform.vs", "Shaders/05Transform.fs");
+	camera = GLcamera(shader, glm::vec3(0, 0, -3), glm::vec3(0, 0, 0));
+
+	// Texture Repeat:
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Use Nearest Neighbor Filtering (pixelated):
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	loadImage("Textures/Container.jpg",  texture1);
+	loadImage("Textures/Illuminati.jpg", texture2);
+
+	shader.use();
+	shader.setInt("uTexture1", 0);
+	shader.setInt("uTexture2", 1); // or with shader class
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	// We want to use the VAO:
+	glBindVertexArray(VAO);
+
+	// Bind VBO and copy verticies data into buffer:
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Set vertex attribute (layout (location = 0) in vec3 aPos):
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Set vertex attribute (layout (location = 1) in vec3 aColor):
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// Set vertex attribute (layout (location = 1) in vec3 aTexCoord):
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+}
+
+void onShutdown()
+{
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+}
+
+void display()
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	shader.use();
+
+	camera.setRotation(glm::vec3(0, (float)glfwGetTime() * 50, 0));
+
+	camera.update();
+
+    // Do Perspective:
+	static GLint width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glm::mat4 projection = glm::perspective<GLfloat>(glm::radians(45.0f), width / (GLfloat)height, 0.1f, 100.0f);
+	unsigned int transformLoc = glGetUniformLocation(shader.ID, "projection");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	// Draw objects:
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glBindVertexArray(VAO);
+
+	// Draw 3 triangles and position them accordingly to index:
+	for (GLint i = 0; i < 3; i++)
+	{
+		glm::mat4 world;
+		world = glm::rotate(world, glm::radians(i * 120.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		world = glm::translate(world, glm::vec3(0.0f, 0.0f, 0.145f));
+		world = glm::rotate(world, glm::radians(-16.75f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		unsigned int uWorld = glGetUniformLocation(shader.ID, "world");
+		glUniformMatrix4fv(uWorld, 1, GL_FALSE, glm::value_ptr(world));
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
+}
+
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
     glViewport(0, 0, width, height);
+}
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    glMatrixMode (GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.0, width/(GLfloat)height, 0.1, 500.0);
-    glMatrixMode (GL_MODELVIEW);
-    glLoadIdentity();
-
-    angle--;
-
-    if (angle > 360)
-    	angle -= 360;
-    else
-	if (angle < 0)
-		angle += 360;
-    glPointSize(4);
-
-    glPushMatrix();
-    	glLoadIdentity();
-    	glTranslatef(0,0,-2.2);
-		glRotatef(angle, 0, 1, 0);
-		glBegin(GL_POINTS);
-			for(i = 0; i < LEN(vertices); i++)
-			{
-				//glColor3fv(colors[i]);
-				glVertex3fv(vertices[i]);
-			}
-		glEnd();
-    glPopMatrix();
-
-    glFlush();
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 
 int main(int argc, char** argv)
 {
-	GLFWwindow* window;
-
-	glfwSetErrorCallback(errorCallback);
-
-	// Initialize GLFW:
 	if (glfwInit() == GLFW_FALSE)
-		return 0;
+		return GLFW_FALSE;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	window = glfwCreateWindow(320, 240, "Window", NULL, NULL);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	window = glfwCreateWindow(320, 240, "Texture", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
-		return 0;
+		return GLFW_FALSE;
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // VSync?
+
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	glewExperimental = GL_TRUE; // Fixes a weird crash due to old GLEW version;
+	if(glewInit() != GLEW_OK)	// Version 1.13.0: glewGetString(GLEW_VERSION);
+		throw std::runtime_error("glewInit failed");
+
+	onStart();
 
 	while(!glfwWindowShouldClose(window))
 	{
-        glfwGetFramebufferSize(window, &width, &height);
+		// Get Inputs:
+		processInput(window);
 
-        display();
+		// Rendering here:
+		display();
 
-		glfwSwapBuffers(window);
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
+
+	onShutdown();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
-    return 0;
+    return GLFW_TRUE;
 }
